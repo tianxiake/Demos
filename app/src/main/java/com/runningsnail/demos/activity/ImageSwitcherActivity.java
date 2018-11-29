37a76e7bbc8d731848c3b998ac68c74bb9e24929
liuyongjie <liuyj@chances.com.cn>
2018-11-29 13:32:43 +0800

    TextView上自带添加图片

---
+++ app/src/main/java/com/runningsnail/demos/activity/ImageSwitcherActivity.java
@@ -0,0 +1,18 @@
+package com.runningsnail.demos.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.runningsnail.demos.R;
+
+/**
+ * ImageSwitcher实现图片的切换
+ */
+public class ImageSwitcherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_switcher);
+    }
+}
